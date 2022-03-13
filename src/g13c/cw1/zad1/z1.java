d63eaec711d0650d0ff5dca3645c9178647ea720
Monika Syrzycka <s26124@pjwstk.edu.pl>
2022-03-13 17:37:09 +0100

    start

---
+++ src/g13c/cw1/zad1/z1.java
@@ -0,0 +1,15 @@
+package g13c.cw1.zad1;
+import java.util.*;
+
+public class z1 {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+
+
+    }
+}
