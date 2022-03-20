0dd5862275bb896db0bae42b40f22511b391f55e
s26124 <s26124@pjwstk.edu.pl>
2022-03-20 16:18:03 +0100

    z5

--- src/g13c/cw1/zad5/z5.java
+++ src/g13c/cw1/zad5/z5.java
@@ -1,2 +1,46 @@
-package g13c.cw1.zad5;public class z5 {
+package g13c.cw1.zad5;
+
+public class z5 {
+    public static void main(String[] args) {
+        z5 Array = new z5();
+        Array.generateRandomArray();
+        System.out.print("Generated array: ");
+        Array.printArray();
+
+        System.out.print("Sorted array:    ");
+        Array.sortArray();
+        Array.printArray();
+    }
+
+    private int[] theArray = new int[10];
+    public void generateRandomArray(){
+        for(int i = 0; i < theArray.length; i++){
+            // Random number max=1000, min=100, accuracy: 50
+            theArray[i] = 50*((int)(Math.random()*(20-2))+2);
+        }
+    }
+
+    public void swapValues(int a, int b){
+        int temp = theArray[a];
+        theArray[a] = theArray[b];
+        theArray[b] = temp;}
+
+    public void printArray(){
+        for(int i = 0; i < theArray.length; i++){
+            System.out.print("| " + theArray[i] + " ");
+        }
+        System.out.println("|");}
+
+        public void sortArray(){
+            for (int i=0; i<theArray.length;i++){
+                int min = i;
+                for (int j = i; j < theArray.length; j++){
+                    if (theArray[min]>theArray[j]){
+                        min = j;
+                    }
+                }
+                swapValues(i,min);
+            }
+        }
+
 }
